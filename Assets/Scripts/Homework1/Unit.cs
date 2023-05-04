a4dce2145c765cb3796bd1b80a0794208821d341
aquamarin102 <97702538+aquamarin102@users.noreply.github.com>
2023-05-04 19:49:55 +0300

    homework1
    coroutine
    async/await

---
+++ Assets/Scripts/Homework1/Unit.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private  int _health = 50;
+    private const  int  _maxHealth = 100;
+
+    private void Start()
+    {
+        ReceiveHealing();
+    }
+
+    private void ReceiveHealing()
+    {
+        StartCoroutine(Regeneration(0.5f, 5));
+    }
+    private IEnumerator Regeneration(float waitTime,   int valueHpRegeneration)
+    {
+        float regenerationTime = 3f;
+        float elapsedTime = 0f;
+        
+        while (_health < _maxHealth)
+        {
+            if (elapsedTime == regenerationTime)
+                StopAllCoroutines();
+            
+            yield return new WaitForSeconds(waitTime);
+            elapsedTime += waitTime;
+            _health += valueHpRegeneration;
+
+            if (_health > _maxHealth)
+            {
+                _health = _maxHealth;
+                yield break;
+            }
+            Debug.Log(_health);
+        }
+    }
+}
